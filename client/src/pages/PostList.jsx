c081065728a5f42bffd326cf7fa7f1da6c9742f1
Silvia Chen <silviachen@Silvia-Chens-MacBook-Pro-1292.local>
2024-01-28 16:28:25 -0800

    filter - get all the posts that match the query and userId

--- client/src/pages/PostList.jsx
+++ client/src/pages/PostList.jsx
@@ -1,9 +1,18 @@
-import { Link, useLoaderData } from "react-router-dom"
+import { Form, Link, useLoaderData } from "react-router-dom"
 import { getPosts } from "../api/posts"
 import { PostCard } from "../components/PostCard"
+import { useEffect, useRef } from "react"
 
 function PostList() {
-  const posts = useLoaderData()
+  const {
+    posts,
+    searchParams: { query },
+  } = useLoaderData()
+  const queryRef = useRef()
+
+  useEffect(() => {
+    queryRef.current.value = query
+  }, [query])
 
   return (
     <>
@@ -15,6 +24,33 @@ function PostList() {
           </Link>
         </div>
       </h1>
+
+      <Form className="form mb-4">
+        <div className="form-row">
+          <div className="form-group">
+            <label htmlFor="query">Query</label>
+            <input type="search" name="query" id="query" ref={queryRef} />
+          </div>
+          {/* <div className="form-group">
+            <label htmlFor="userId">Author</label>
+            <select type="search" name="userId" id="userId">
+              <option value="">Any</option>
+              <option value="1">Leanne Graham</option>
+              <option value="2">Ervin Howell</option>
+              <option value="3">Clementine Bauch</option>
+              <option value="4">Patricia Lebsack</option>
+              <option value="5">Chelsey Dietrich</option>
+              <option value="6">Mrs. Dennis Schulist</option>
+              <option value="7">Kurtis Weissnat</option>
+              <option value="8">Nicholas Runolfsdottir V</option>
+              <option value="9">Glenna Reichert</option>
+              <option value="10">Clementina DuBuque</option>
+            </select>
+          </div> */}
+          <button className="btn">Filter</button>
+        </div>
+      </Form>
+
       <div className="card-grid">
         {posts.map((post) => (
           <PostCard key={post.id} {...post} />
@@ -24,8 +60,14 @@ function PostList() {
   )
 }
 
-function loader({ request: { signal } }) {
-  return getPosts({ signal })
+// request: {url} is going to contain query string from the form submit
+async function loader({ request: { signal, url } }) {
+  const searchParams = new URL(url).searchParams
+  const query = searchParams.get("query")
+  const filterParams = { q: query } // depends on API's setting
+  const posts = getPosts({ signal, params: filterParams })
+
+  return { posts: await posts, searchParams: { query } }
 }
 
 export const postListRoute = {
